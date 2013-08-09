11b64bbfef8e41eb4191f2309d23345c962f6617
Anthony Severini <FluffyCode@gmail.com>
2013-08-09 09:50:09 -0700

    ???

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+	def new
+		@user = User.new
+	end
+
+	def index
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+		def app_params
+			params.require(:user).permit(:name, :email, :password, :password_confirmation)
+		end
+
 end
