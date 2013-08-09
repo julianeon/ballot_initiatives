0bfbed216d154838d12cc1c6d3df7a626deaaeba
Anthony Severini <FluffyCode@gmail.com>
2013-08-08 19:52:25 -0700

    Rewrite callback downcasing User.email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 	has_secure_password
 
 	# Callbacks
-	before_save { |user| user.email = email.downcase }
+	before_save { self.email.downcase! }
 
 	# Validations
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
