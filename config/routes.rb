71274b37c905d4271bfbac2ef39673da5c7ff57b
Anthony Severini <FluffyCode@gmail.com>
2013-08-14 12:43:46 -0700

    Configure routes for signup, signin and signout paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 BallotInitiatives::Application.routes.draw do
-  resources :initiatives
-  resources :users
+	resources :initiatives
+	resources :users
+	resources :sessions, only: [:new, :create, :destroy]
 
-  root 'initiatives#index'
+	# Root path
+	root 'initiatives#index'
+
+	# Signup, sign-in and sign-out paths
+	get '/signup',  to: 'users#new'
+	get '/signin',  to: 'sessions#new'
+	get '/signout', to: 'sessions#destroy', via: :delete
 end
