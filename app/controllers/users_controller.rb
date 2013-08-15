645ef6c428d823ec3bc2ed76e87c23689bb670c0
Anthony Severini <FluffyCode@gmail.com>
2013-08-15 12:22:01 -0700

    Sign-in the user upon signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 		@user = User.new(params[:user])
 
 		if @user.save
+			sign_in @user
 			flash[:success] = "Signup successful - welcome to SF Initiatives!"
 			redirect_to @user
 		else
