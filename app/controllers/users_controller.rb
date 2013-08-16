4bca904000c6128289688cbd280de5fdb230039a
Anthony Severini <FluffyCode@gmail.com>
2013-08-15 22:24:11 -0700

    Add callback to ensure only admin users can access the users#delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-	before_filter :signed_in_user, only: [:edit, :update, :index]
+	before_filter :signed_in_user, only: [:edit, :update, :index, :destroy]
 	before_filter :correct_user,   only: [:edit, :update]
+	before_filter :admin_user,     only: [:destroy]
 
 	def new
 		@user = User.new
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def destroy
+		User.find(params[:id]).destroy
+		flash[:success] = "User destroyed."
+		redirect_to users_path
+	end
+
 	private
 		# Rails 4 replacement for attr_accessible in User model
 		def app_params
@@ -60,6 +67,10 @@ class UsersController < ApplicationController
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_path) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_path) unless current_user.admin?
 		end
 
 end
