ca1f0610b9d73fac40be4c079592c3e5540e8bb6
Anthony Severini <FluffyCode@gmail.com>
2013-08-16 00:28:29 -0700

    Rework users#create and user_params to make rails 4 happy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 	end
 
 	def create
-		@user = User.new(params[:user])
+		@user = User.new(user_params)
 
 		if @user.save
 			sign_in @user
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
 
 	private
 		# Rails 4 replacement for attr_accessible in User model
-		def app_params
+		def user_params
 			params.require(:user).permit(:name, :email, :password, :password_confirmation)
 		end
 
