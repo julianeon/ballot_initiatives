787126523fe51dc0cf0605f0090975b1b8fb368d
Anthony Severini <FluffyCode@gmail.com>
2013-08-20 16:48:42 -0700

    Touchup indentation, reorder actions

--- app/controllers/initiatives_controller.rb
+++ app/controllers/initiatives_controller.rb
@@ -1,75 +1,75 @@
 class InitiativesController < ApplicationController
-  before_action :set_initiative, only: [:show, :edit, :update, :destroy]
-  before_filter :signed_in_user, only: [:new, :create, :destroy]
+	before_action :set_initiative, only: [:show, :edit, :update, :destroy]
+	before_filter :signed_in_user, only: [:new, :create, :destroy]
 
-  # GET /initiatives
-  # GET /initiatives.json
-  def index
-    @initiatives = Initiative.all.page(params[:page])
-  end
+	# GET /initiatives
+	# GET /initiatives.json
+	def index
+		@initiatives = Initiative.all.page(params[:page])
+	end
 
-  # GET /initiatives/1
-  # GET /initiatives/1.json
-  def show
-  end
+	# GET /initiatives/1
+	# GET /initiatives/1.json
+	def show
+	end
 
-  # GET /initiatives/new
-  def new
-    @initiative = Initiative.new
-  end
+	# GET /initiatives/new
+	def new
+		@initiative = Initiative.new
+	end
+	
+	# POST /initiatives
+	# POST /initiatives.json
+	def create
+		@initiative = Initiative.new(initiative_params)
 
-  # GET /initiatives/1/edit
-  def edit
-  end
+		respond_to do |format|
+			if @initiative.save
+				format.html { redirect_to @initiative, notice: 'Initiative was successfully created.' }
+				format.json { render action: 'show', status: :created, location: @initiative }
+			else
+				format.html { render action: 'new' }
+				format.json { render json: @initiative.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # POST /initiatives
-  # POST /initiatives.json
-  def create
-    @initiative = Initiative.new(initiative_params)
+	# GET /initiatives/1/edit
+	def edit
+	end
 
-    respond_to do |format|
-      if @initiative.save
-        format.html { redirect_to @initiative, notice: 'Initiative was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @initiative }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @initiative.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# PATCH/PUT /initiatives/1
+	# PATCH/PUT /initiatives/1.json
+	def update
+		respond_to do |format|
+			if @initiative.update(initiative_params)
+				format.html { redirect_to @initiative, notice: 'Initiative was successfully updated.' }
+				format.json { head :no_content }
+			else
+				format.html { render action: 'edit' }
+				format.json { render json: @initiative.errors, status: :unprocessable_entity }
+			end
+		end
+	end
 
-  # PATCH/PUT /initiatives/1
-  # PATCH/PUT /initiatives/1.json
-  def update
-    respond_to do |format|
-      if @initiative.update(initiative_params)
-        format.html { redirect_to @initiative, notice: 'Initiative was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @initiative.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+	# DELETE /initiatives/1
+	# DELETE /initiatives/1.json
+	def destroy
+		@initiative.destroy
+		respond_to do |format|
+			format.html { redirect_to initiatives_url }
+			format.json { head :no_content }
+		end
+	end
 
-  # DELETE /initiatives/1
-  # DELETE /initiatives/1.json
-  def destroy
-    @initiative.destroy
-    respond_to do |format|
-      format.html { redirect_to initiatives_url }
-      format.json { head :no_content }
-    end
-  end
+	private
+		# Use callbacks to share common setup or constraints between actions.
+		def set_initiative
+			@initiative = Initiative.find(params[:id])
+		end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_initiative
-      @initiative = Initiative.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def initiative_params
-      params.require(:initiative).permit(:election_date, :prop_letter, :title, :description, :pass_fail, :yes_count, :no_count, :percent_required, :measure_type, :initiator, :scan_url)
-    end
+		# Never trust parameters from the scary internet, only allow the white list through.
+		def initiative_params
+			params.require(:initiative).permit(:election_date, :prop_letter, :title, :description, :pass_fail, :yes_count, :no_count, :percent_required, :measure_type, :initiator, :scan_url)
+		end
 end
