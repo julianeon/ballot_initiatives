ba37f70731ed198b059992d1397a766061b0f3e3
Anthony Severini <FluffyCode@gmail.com>
2013-08-19 17:10:09 -0700

    Redirect signed-in users (away from users#new and users#create, to root)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,26 @@ class UsersController < ApplicationController
 	before_filter :admin_user,     only: [:destroy]
 
 	def new
-		@user = User.new
+		if signed_in?
+			redirect_to root_path
+		else
+			@user = User.new
+		end
 	end
 
 	def create
-		@user = User.new(user_params)
-
-		if @user.save
-			sign_in @user
-			flash[:success] = "Signup successful - welcome to SF Initiatives!"
-			redirect_to @user
+		if signed_in?
+			redirect_to root_path
 		else
-			render 'new'
+			@user = User.new(user_params)
+
+			if @user.save
+				sign_in @user
+				flash[:success] = "Signup successful - welcome to SF Initiatives!"
+				redirect_to @user
+			else
+				render 'new'
+			end
 		end
 	end
 
