3027f93c10f3de34b4ab0d009e65fc93aa5d2155
Anthony Severini <FluffyCode@gmail.com>
2013-08-21 18:33:09 -0700

    Allow admins to visit non-admin user profiles (standard users can still only view their own profile)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,7 +74,7 @@ class UsersController < ApplicationController
 		# Define correct_user - ensure the user can only edit/update their own profiles
 		def correct_user
 			@user = User.find(params[:id])
-			redirect_to(root_path) unless current_user?(@user)
+			redirect_to(root_path) unless current_user?(@user) || current_user.admin?
 		end
 
 		def admin_user
